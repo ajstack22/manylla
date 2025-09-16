ebbedfa77bcc18d9f5ad98a344bc940a823aad87
Adam Stack <stackadamj@gmail.com>
2025-09-16 11:24:28 -0500

    refactor: Remove dead Profile components and fix date picker
    - Removed unused Profile components that were replaced by UnifiedApp
    - Fixed date picker on web to use HTML5 input in ProfileEditForm
    - Re-enabled webpack filesystem cache for proper file watching
    - Cleaned up incorrect webpack alias pointing to non-existent file
    
    This resolves the date picker issue by focusing on the actual component
    being used (ProfileEditForm in UnifiedApp) rather than the unused
    ProfileEditDialog component.

--- src/components/Profile/index.js
+++
@@ -1,5 +0,0 @@
-// Use the unified .js files for all platforms
-export { CategorySection } from "./CategorySection";
-export { ProfileOverview } from "./ProfileOverview";
-export { ProfileEditDialog } from "./ProfileEditDialog";
-export { ProfileCreateDialog } from "./ProfileCreateDialog";
